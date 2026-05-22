b3dc59f01bc2e33303acf18f9961f849ae8fe827
IvanGv12 <ivangomva13@gmail.com>
2026-05-21 18:06:54 -0600

    railway config

--- vite.config.js
+++ vite.config.js
@@ -2,41 +2,58 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
+// URL del backend
+const backendUrl =
+  process.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
   ],
+
   server: {
+    host: '0.0.0.0',
+
+    // Permitir Railway
+    allowedHosts: true,
+
     proxy: {
-      "/sanctum": {
-        target: "http://127.0.0.1:8000",
+      '/sanctum': {
+        target: backendUrl,
         changeOrigin: true,
         secure: false,
       },
-      "/api": {
-        target: "http://127.0.0.1:8000",
+
+      '/api': {
+        target: backendUrl,
         changeOrigin: true,
         secure: false,
       },
-      "/login": {
-        target: "http://127.0.0.1:8000",
+
+      '/login': {
+        target: backendUrl,
         changeOrigin: true,
         secure: false,
       },
-      "/register": {
-        target: "http://127.0.0.1:8000",
+
+      '/register': {
+        target: backendUrl,
         changeOrigin: true,
         secure: false,
       },
-      "/logout": {
-        target: "http://127.0.0.1:8000",
+
+      '/logout': {
+        target: backendUrl,
         changeOrigin: true,
         secure: false,
       },
     },
   },
+
   test: {
     environment: 'jsdom',
     globals: true,
